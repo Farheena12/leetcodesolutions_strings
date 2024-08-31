a81cc0b0239a7ea35551f6e27bc71321e6302305
Farheena12 <70895688+Farheena12@users.noreply.github.com>
2024-08-31 13:02:04 +0530

    Create maxwordsSentence.py

---
+++ maxwordsSentence.py
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max([len(s.split()) for s in sentences])
+        
