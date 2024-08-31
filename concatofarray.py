c44b4a2a06458157a7b1025cd30fa6d5dff882c5
Farheena12 <70895688+Farheena12@users.noreply.github.com>
2024-08-31 20:25:19 +0530

    Create concatofarray.py

---
+++ concatofarray.py
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/concatenation-of-array/submissions/1374287280/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2
+        
